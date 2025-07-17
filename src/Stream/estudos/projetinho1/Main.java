afd6992657fffe1e86a93fabe36d20e1e2e44cff
cmxrenato <renatoaraujoleal@gmail.com>
2025-07-16 23:52:37 -0300

    trabalho no projetinho1

--- src/Stream/estudos/projetinho1/Main.java
+++ src/Stream/estudos/projetinho1/Main.java
@@ -16,14 +16,49 @@ public class Main{
 			    new Pessoa("Marcos", 52),
 			    new Pessoa("Sandra", 55),
 			    new Pessoa("Mateus", 18),
-			    new Pessoa("Sandra", 54)
+			    new Pessoa("Bruna", 54)
 			);
+		//imprime tudo
+		//pessoas.forEach(System.out::println);
 		
 		System.out.println("Pessoas com mais 30 anos: ");
 		pessoas
 		.stream()
 		.filter(p -> p.getIdade() > 30)
 		.forEach(System.out::println);
+		
+		System.out.println("Média das idades:");
+		double media = pessoas
+				.stream()
+				.mapToInt(Pessoa::getIdade)
+				.average()
+				.orElse(0);
+		System.out.println(media);
+		
+		System.out.println("Nomes ordenados:");
+		List<String> nomes = pessoas.stream()
+		.map(Pessoa::getNome)
+		.sorted()
+		.collect(Collectors.toList());
+		
+		System.out.println(nomes);
+		
+		System.out.println("Agrupamento por idade:");
+		Map<Integer,List<Pessoa>> agrupadoPorIdade = pessoas.stream()
+				.collect(Collectors.groupingBy(Pessoa::getIdade));
+		
+		agrupadoPorIdade.forEach((idade,grupo) -> {
+			System.err.println("Idade: "+idade+" : "+grupo);
+			
+		});
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 }
